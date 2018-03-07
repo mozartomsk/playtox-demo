2aef743c1ff1ee7c92034fde51972b74a643e678
Pavel Konstantinov <pavel.konstantinov@tradeshift.com>
2018-03-07 11:59:38 +0700

    Added application structure

---
+++ wicket-web/src/main/java/ru/mozar/playtox/demo/wicket/home/HomePage.java
@@ -0,0 +1,13 @@
+package ru.mozar.playtox.demo.wicket.home;
+
+import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
+import org.apache.wicket.markup.html.WebPage;
+
+@WicketHomePage
+public class HomePage extends WebPage {
+
+
+    //...page code
+
+
+}
